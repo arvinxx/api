30458c8b821bfdd8a5a0d4370b4b31355b98b502
arvinxx <arvinx@foxmail.com>
2021-02-26 18:14:55 +0800

    :white_check_mark: test: 添加测试

---
+++ api/tests/stocks.spec.ts
@@ -0,0 +1,42 @@
+/**
+ * @jest-environment node
+ */
+
+import Token from '../stocks/token';
+import Item from '../stocks/item/[code]';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+describe('股票数据', () => {
+  it('获取雪球 token', async () => {
+    const { data, success } = await Token(
+      <VercelRequest>(<unknown>{}),
+      <VercelResponse>(<unknown>{
+        json: () => {},
+      }),
+    );
+
+    expect(success).toBeTruthy();
+    expect(data.token).toHaveLength(40);
+  });
+  it('请求雪球数据', async () => {
+    const { data, success } = await Token(
+      <VercelRequest>(<unknown>{}),
+      <VercelResponse>(<unknown>{
+        json: () => {},
+      }),
+    );
+    if (!success) return;
+
+    const res = await Item(
+      <VercelRequest>(<unknown>{
+        query: { code: 'SH600519', token: data.token },
+      }),
+      <VercelResponse>(<unknown>{
+        json: () => {},
+      }),
+    );
+
+    expect(res.data.name).toEqual('贵州茅台');
+    expect(res.data.symbol).toEqual('SH600519');
+  });
+});
---
+++ api/tests/zotero.spec.ts
@@ -0,0 +1,25 @@
+import BooksList from '../books/[year]';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+describe('Zotero 书单', () => {
+  it('2020年书单', async () => {
+    const { raw } = await BooksList(
+      <VercelRequest>(<unknown>{ query: { year: '2020' } }),
+      <VercelResponse>(<unknown>{
+        json: () => {},
+      }),
+    );
+    expect(raw.key).toEqual('22VU3NNR');
+    expect(raw.name).toEqual('2020年书单');
+  });
+  it('2021年书单', async () => {
+    const { raw } = await BooksList(
+      <VercelRequest>(<unknown>{ query: { year: '2021' } }),
+      <VercelResponse>(<unknown>{
+        json: () => {},
+      }),
+    );
+    expect(raw.key).toEqual('GIE4NH9Q');
+    expect(raw.name).toEqual('2021年书单');
+  });
+});
