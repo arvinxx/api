727a811be7d9949b62cb45dbf6bb4aa6b1d9eeae
arvinxx <arvinx@foxmail.com>
2023-03-10 00:02:15 +0800

    :sparkles: feat: 支持设定 systemMessage

--- api/ai/chat.ts
+++ api/ai/chat.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { ProxyAgent, setGlobalDispatcher, fetch } from 'undici';
+import { ProxyAgent, setGlobalDispatcher } from 'undici';
 import { ChatGPTAPI } from './_chatgpt';
 
 const IS_DEV = process.env.NODE_ENV === 'development';
@@ -8,6 +8,7 @@ const HTTPS_PROXY = process.env.HTTPS_PROXY as string;
 
 export default async function handler(req: VercelRequest, response: VercelResponse) {
   const messages = req.body.messages;
+  const systemMessage = req.body.systemMessage;
 
   if (!messages) {
     response.send('No input text');
@@ -19,11 +20,9 @@ export default async function handler(req: VercelRequest, response: VercelRespon
   }
 
   const api = new ChatGPTAPI({
-    systemMessage:
-      '你是擅长做饭的米其林大厨，接下来我会问你很多关于菜谱的问题，请将结果以 html 格式进行输出',
+    systemMessage,
     apiKey,
     debug: IS_DEV,
-    fetch: fetch as any,
   });
 
   const result = await api.sendMessage(messages, {
